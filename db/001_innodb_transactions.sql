12c58d30cd091a244253cb293c5df4e247e98c8f
wshunli <2440235075@qq.com>
2026-06-25 00:10:36 +0800

    feat:批量排课逻辑优化

--- db/001_innodb_transactions.sql
+++
@@ -1,5 +0,0 @@
--- 执行前务必完整备份数据库，并先在测试库验证。
--- 历史表为 MyISAM；只有切换到 InnoDB 后，多表事务的提交与回滚才真正生效。
-ALTER TABLE `borrow` ENGINE = InnoDB;
-ALTER TABLE `submit` ENGINE = InnoDB;
-ALTER TABLE `console` ENGINE = InnoDB;
